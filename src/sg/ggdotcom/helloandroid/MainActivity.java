152bc175a6387dfc96d0e97793b76d8a763da004
Tai Li <tltan86@gmail.com>
2013-06-01 12:08:09 +0800

    Removed dialog messages. Added demo button and edit field.

--- src/sg/ggdotcom/helloandroid/MainActivity.java
+++ src/sg/ggdotcom/helloandroid/MainActivity.java
@@ -5,14 +5,12 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
-public class HelloAndroidActivity extends Activity {
+public class MainActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
-		
-		
-        // setContentView(R.layout.main);
+        setContentView(R.layout.main);
 
         /*
         DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
